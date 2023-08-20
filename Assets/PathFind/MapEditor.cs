20c1dda717f1e41207d0c87a3cf02b4c9c371114
Mikyuu <1428590416@qq.com>
2023-08-20 22:10:51 +0800

    #0 新建文件夹

---
+++ Assets/PathFind/MapEditor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace PathFind
+{
+    [ExecuteInEditMode]
+    public class MapEditor : MonoBehaviour
+    {
+       void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
